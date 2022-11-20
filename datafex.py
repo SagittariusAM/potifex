40f4ad4f12f2092c78b9f92732326d39bdbdc9e4
mohammed <zeggafmohah@gmail.com>
2022-11-20 19:30:01 +0100

    potentially final draft

--- datafex.py
+++ datafex.py
@@ -2,9 +2,11 @@ from __future__ import unicode_literals
 import os
 import json
 from requests import get
-import youtube_dl
-from youtube_dl import YoutubeDL
+import yt_dlp
+from yt_dlp import YoutubeDL
+from yt_dlp.postprocessor.ffmpeg import FFmpegMetadataPP
 from bs4 import *
+from metadfex import *
 
 YDL_OPTIONS = {'format': 'bestaudio', 'noplaylist':'True'}
 
@@ -19,32 +21,54 @@ def search(arg):
 
     return video
 
-def downloadS(id):
+def downloadS(id, song):
+    outS = song.name + '.%(ext)s'
     ydl_opts = {
         'format': 'bestaudio/best',
+        'quality': '0',
+        'outtmpl': outS,
+        # C:/Users/lenovo/Music/iTunes/iTunes Media/Automatically Add to iTunes/
         'postprocessors': [{
             'key': 'FFmpegExtractAudio',
             'preferredcodec': 'mp3',
             'preferredquality': '192',
         }],
     }
-    with youtube_dl.YoutubeDL(ydl_opts) as ydl:
+    with yt_dlp.YoutubeDL(ydl_opts) as ydl:
         ydl.download([f"https://www.youtube.com/watch?v={id}"])
+        
 
 def getS(thestuff):
     with open(f'{thestuff}.json', 'r') as f:
         datafex = json.load(f)
-
     for dictn in datafex["tracks"]["items"]:
-        song = Song(dictn)
+        try:
+            a2 = dictn["track"]["artists"][1]["name"]
+            a1 = dictn["track"]["artists"][0]["name"]
+            artists = f"{a1} & {a2}"
+        except: 
+            artists = dictn["track"]["artists"][0]["name"]
+        song = Song(dictn, artists, datafex)
+        print(f"Name: {song.name}\nAlbum: {song.album}\nArtist: {song.artist}")
         # name = (song.name).replace(" ", "+").replace("&", "%26").replace(":", "%3A")
         # print(f"https://www.youtube.com/results?search_query={name}")
         m = search(song.name)
         idSong = m["id"]
         title = m["title"]
-        input(f"Proceed to download ? {title}")
-        downloadS(idSong)
+        # input(f"Proceed to download ? {title}")
+        downloadS(idSong, song)
+        metadata(song)
+        moveSong(song)
 
 class Song:
-    def __init__(self, name):
-        self.__dict__ = name["track"]
+    def __init__(self, sData, sArtists, datafex):
+        self.id = sData["track"]["id"]
+        self.name = sData["track"]["name"]
+        self.album = sData["track"]["album"]["name"]
+        self.artist = sArtists
+        self.albumartist = datafex["name"]
+        self.track = sData["track"]["track_number"]
+        self.imageURL = sData["track"]["album"]["images"][0]["url"]
+
+
+getS("thestuff")
